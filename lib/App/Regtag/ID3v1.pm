9988318d90d85891c2d40917ebff2c6a7ee8d769
Sawyer X <xsawyerx@cpan.org>
2013-06-28 01:32:03 +0300

    ID3 refactoring

--- lib/App/Regtag/ID3v1.pm
+++ lib/App/Regtag/ID3v1.pm
@@ -2,59 +2,20 @@ package App::Regtag::ID3v1;
 # ABSTRACT: Write ID3v1 tags
 
 use Moo;
-use MP3::Mplib;
-use Term::ANSIColor;
-use IO::Prompt::Tiny 'prompt';
 
-has tags => (
-    is      => 'ro',
+with 'App::Regtag::Role::ID3';
+
+has '+tags' => (
     default => sub { [ qw<artist title album year track type comment> ] },
 );
 
-has tag_alias => (
-    is      => 'ro',
+has '+tag_alias' => (
     default => sub { {
         name   => 'title',
         genre  => 'type',
         number => 'track',
     } },
 );
-
-sub run {
-    my $self = shift;
-    my $data = shift;
-
-    $self->ask_for_confirmation($data);
-    $self->apply_changes($data);
-}
-
-sub ask_for_confirmation {
-    my $self = shift;
-    my $data = shift;
-    my @tags = @{ $self->tags };
-
-    foreach my $file ( keys %{$data} ) {
-        print colored( '* ', 'green' ), "$file:\n";
-
-        foreach my $tag (@tags) {
-            my $tag_content = $data->{$file}{ uc $tag };
-            defined $tag_content or next;
-
-            printf "  %-7s %s\n",
-                  ( ucfirst $tag ) . ':',
-                  colored( $tag_content, 'blue' );
-        }
-    }
-
-    my $answer = prompt(
-        colored( 'Would you like to apply these tags [y/N]?', 'yellow' )
-    );
-
-    if ( $answer ne 'y' && $answer ne 'Y' ) {
-        print "No changes made.\n";
-        exit;
-    }
-}
 
 sub show_tags {
     my $self  = shift;
@@ -75,28 +36,6 @@ Capture Name(s)    Alias     Meaning                   ID3 Frame
 When both name and alias are provided, the name takes precedence.
 _TAGS
 
-}
-
-sub apply_changes {
-    my $self = shift;
-    my $data = shift;
-
-    foreach my $file ( keys %{$data} ) {
-        print colored( '* ', 'green' ), "$file ... ";
-
-        my $mp3 = MP3::Mplib->new($file);
-
-        # we always strip what we do
-        $mp3->del_v1tag;
-        $mp3->del_v2tag;
-
-        my $tag_data = $data->{$file};
-        if ( $mp3->set_v1tag($tag_data) ) {
-            print '[', colored( 'OK', 'green' ), "]\n";
-        } else {
-            print '[', colored( 'FAIL', 'red' ), "]\n";
-        }
-    }
 }
 
 1;
--- lib/App/Regtag/ID3v2.pm
+++ lib/App/Regtag/ID3v2.pm
@@ -2,28 +2,14 @@ package App::Regtag::ID3v2;
 # ABSTRACT: Write ID3v2 tags
 
 use Moo;
-use MP3::Mplib;
-use Term::ANSIColor;
-use IO::Prompt::Tiny 'prompt';
 
-has tags => (
-    is      => 'ro',
+has '+tags' => (
     default => sub { [ qw<> ] },
 );
 
-has tag_alias => (
-    is      => 'ro',
+has '+tag_alias' => (
     default => sub { {} },
 );
-
-sub run {
-    my $self = shift;
-    my $data = shift;
-
-    # refactor
-    #$self->ask_for_confirmation($data);
-    $self->apply_changes($data);
-}
 
 sub show_tags {
     my $self  = shift;
@@ -37,28 +23,6 @@ Capture Name(s)    Alias     Meaning                   ID3 Frame
 When both name and alias are provided, the name takes precedence.
 _TAGS
 
-}
-
-sub apply_changes {
-    my $self = shift;
-    my $data = shift;
-
-    foreach my $file ( keys %{$data} ) {
-        print colored( '* ', 'green' ), "$file ... ";
-
-        my $mp3 = MP3::Mplib->new($file);
-
-        # we always strip what we do
-        $mp3->del_v1tag;
-        $mp3->del_v2tag;
-
-        my $tag_data = $data->{$file};
-        if ( $mp3->set_v2tag($tag_data) ) {
-            print '[', colored( 'OK', 'green' ), "]\n";
-        } else {
-            print '[', colored( 'FAIL', 'red' ), "]\n";
-        }
-    }
 }
 
 1;
